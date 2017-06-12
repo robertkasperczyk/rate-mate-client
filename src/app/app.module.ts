a2bf9c4ddd2d577758e691fc675e74367fa0b13b
Robert Kasperczyk <robertkasperczyk@gmail.com>
2017-06-12 17:45:11 +0200

    stub products and product-details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProductsComponent } from './products/products.component';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './footer/footer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {ProductsComponent} from './products/products.component';
+import {RouterModule, Routes} from "@angular/router";
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+
+const appRoutes: Routes = [
+  {path: 'products', component: ProductsComponent},
+  {path: 'product/:id', component: ProductDetailComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
