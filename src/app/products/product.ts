f447889efde1a6ef3e243e5ed23254143b2af9a4
Robert Kasperczyk <robertkasperczyk@gmail.com>
2017-06-12 21:16:14 +0200

    read products from server and displaying them

---
+++ src/app/products/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+  _id: string;
+  name: string;
+  description: string;
+  imagePath: string;
+  powerRating: number;
+  dustRating: number;
+  tasteRating: number;
+  numberOfRatings: number;
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
+import "rxjs/add/operator/map";
+import {Product} from "./product";
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products: Array<Product>;
 
-  constructor() { }
+  constructor(private http: Http) {
+    http.get('http://localhost:3000/products')
+      .map(res => res.json())
+      .subscribe(data => {
+          this.products = data;
+          this.products.forEach(a => a.imagePath = "http://localhost:3000/product/" + a._id + "/" + a.imagePath)
+        },
+        err => console.log(err),
+        () => console.log('success'));
+  }
 
   ngOnInit() {
   }
