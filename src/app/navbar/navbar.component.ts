ce44e2337657ec81a03277fbee40ed033d29c9e4
Katarzyna Rzepecka <katarzyn.rzepecka@gmail.com>
2017-06-19 01:31:56 +0200

    Added active navbar's hover element's color when actual url

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'navbar',
@@ -7,11 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
-  hover_elem='nav_elem_hover'
 
 }
